231d3057c17fd44c4ca78f9379ad002c7030b91f
SomewhatDamaged <not.2.me.damaged@gmail.com>
2026-05-23 15:35:13 +1000

    Move hashes to hashes.json
    Fix 2

--- src/hashcompare.py
+++ src/hashcompare.py
@@ -4,7 +4,7 @@ import traceback
 from json import dumps, load
 
 
-with open('hashcompare.json', 'r') as f:
+with open('hashes.json', 'r') as f:
     hashes = load(f)
 
 class Default(WorkerEntrypoint):
